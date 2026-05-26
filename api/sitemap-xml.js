1fccbb0e6a62ee22ca23822e6dd7f8c6b7a03665
anmar727 <anmar727@gmail.com>
2026-05-26 22:10:36 +0400

    Update sitemap-xml.js

--- api/sitemap-xml.js
+++ api/sitemap-xml.js
@@ -1,21 +1,20 @@
 module.exports = async function handler(req, res) {
-  var baseUrl = "https://www.naturalwellblog.com";
+  var base = "https://www.naturalwellblog.com";
   var today = new Date().toISOString().slice(0, 10);
-
-  var staticPages = [
-    { url: "/", priority: "1.0", changefreq: "daily" },
-    { url: "/about", priority: "0.5", changefreq: "monthly" },
-    { url: "/contact", priority: "0.5", changefreq: "monthly" },
-    { url: "/privacy", priority: "0.3", changefreq: "yearly" },
+  var pages = [
+    { loc: base + "/", priority: "1.0", freq: "daily" },
+    { loc: base + "/about", priority: "0.6", freq: "monthly" },
+    { loc: base + "/contact", priority: "0.4", freq: "monthly" },
+    { loc: base + "/privacy", priority: "0.3", freq: "yearly" },
   ];
-
-  var urls = staticPages.map(function(p) {
-    return "<url><loc>" + baseUrl + p.url + "</loc><lastmod>" + today + "</lastmod><changefreq>" + p.changefreq + "</changefreq><priority>" + p.priority + "</priority></url>";
+  var xml = '<?xml version="1.0" encoding="UTF-8"?>';
+  xml += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
+  pages.forEach(function(p) {
+    xml += "<url><loc>" + p.loc + "</loc><lastmod>" + today + "</lastmod>";
+    xml += "<changefreq>" + p.freq + "</changefreq><priority>" + p.priority + "</priority></url>";
   });
-
-  var sitemap = '<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' + urls.join("") + "</urlset>";
-
-  res.setHeader("Content-Type", "application/xml");
+  xml += "</urlset>";
+  res.setHeader("Content-Type", "application/xml; charset=utf-8");
   res.setHeader("Cache-Control", "public, max-age=3600");
-  res.status(200).send(sitemap);
+  res.status(200).send(xml);
 };
